3649d3a321f5c4356dc68f7745aca9c8edfa7e41
MEmilia01 <mariana_cardozo@euneiz.com>
2026-09-21 13:46:51 +0200

    add canvas

--- Assets/Primerproy/Scripts/CanvasManager.cs
+++ Assets/Primerproy/Scripts/CanvasManager.cs
@@ -39,6 +39,19 @@ public class CanvasManager : MonoBehaviour
     //abrir y cerrar menus
     public void Abririnicio()
     {
-        
+        menuinicio.SetActive(true);
+        menulista.SetActive(false);
+        Menuanadir.SetActive(false);
+        menuborrar.SetActive(false);
+        Mensajepop.SetActive(false);
+    }
+
+    public void Abrirlista()
+    {
+        menuinicio.SetActive(false);
+        menulista.SetActive(true);
+        Menuanadir.SetActive(false);
+        menuborrar.SetActive(false);
+        Mensajepop.SetActive(false);
     }
 }
