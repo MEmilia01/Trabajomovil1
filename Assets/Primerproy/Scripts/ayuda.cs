2801e92a1da384c12ad975f03d3c395e4a7718bf
MEmilia01 <mariana_cardozo@euneiz.com>
2026-09-21 12:28:39 +0200

    add basic scripts

---
+++ Assets/Primerproy/Scripts/ayuda.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ayuda : MonoBehaviour
+{
+    /*
+    variables de los usuarios
+    id(identificair) // nombre // edad
+
+    funciones
+    añadir usuarios, solo nombre y edad
+    eliminar, con el id 
+    ordenar por id o edad
+
+    demas:
+    lista dinamica
+    mensaje hayacente de feedback
+
+    */
+}
